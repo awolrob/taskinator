0164e5df0c5bd6fd7fe7b0e5cc4596c9ed08f86f
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-06-08 18:10:35 -0400

    add reset form feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var taskFormHandler = function(event){
+var taskFormHandler = function (event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  // check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  formEl.reset();
+  
   // package up data as an object
   var taskDataObj = {
-      name: taskNameInput,
-      type: taskTypeInput
+    name: taskNameInput,
+    type: taskTypeInput
   };
   // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
@@ -26,7 +33,7 @@ var createTaskEl = function (taskDataObj) {
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
   // add entire list item to list
-  tasksToDoEl.appendChild(listItemEl) ;
+  tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", taskFormHandler);
